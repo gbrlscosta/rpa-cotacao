b60b9ede8cd556061bfa9a6b919e4f74ca8eaf2b
gbrlscosta <gabriel.costa@germinare.org.br>
2024-06-22 07:32:16 -0300

    subindo rpa e script

---
+++ cotacao_dollar.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+import psycopg2
+import time
+from datetime import datetime
+
+try:
+#conexao db
+    conexao = psycopg2.connect(user="avnadmin", password="AVNS_soL4IKAZqbRQwbx06Jb",
+                                host="devops-postgresql-pipeline-banco-postgresql-devops.e.aivencloud.com",
+                                port="24043",
+                                database="dbcotacao")
+
+    print("Conexão bem-sucedida!")
+except psycopg2.Error as e:
+    print("Erro ao conectar ao banco de dados:", e)
+cursor = conexao.cursor()
+
+# Abrir o navegador Chrome
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
+time.sleep(4)
+
+# Abra a página desejada
+driver.get("https://www.google.com/search?q=dolar&rlz=1C1GCEU_pt-BRBR1086BR1086&oq=dolar+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTINCAEQABiDARixAxiABDINCAIQABiDARixAxiABDINCAMQABiDARixAxiABDINCAQQABiDARixAxiABDINCAUQABiDARixAxiABDINCAYQABiDARixAxiABDINCAcQABiDARixAxiABDINCAgQABiDARixAxiABDINCAkQABiDARixAxiABNIBCDIzMTJqMGo5qAIAsAIB&sourceid=chrome&ie=UTF-8")
+time.sleep(2)
+
+# Encontrar elemento
+dollar_element = driver.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]')
+dollar = dollar_element.text
+dollar = dollar.replace(',', '.')
+dollar = float(dollar)
+
+# Horario atual
+data = datetime.now().strftime("%Y-%m-%d")
+horario = datetime.now().strftime("%H:%M:%S")
+
+try:
+    query = """
+    CALL inserir_cotacao_dolar(%s, %s, %s)
+    """
+    valores = (data, horario, dollar)
+    cursor.execute(query, valores)
+    conexao.commit()  # Commit the transaction if needed
+    print('Sucesso!')
+except Exception as e:
+    print(f"Error executing stored procedure: {e}")
+    conexao.rollback()  # Rollback in case of error
+finally:
+    cursor.close()
+    conexao.close()
